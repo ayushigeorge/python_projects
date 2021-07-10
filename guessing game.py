f10180e62323a3396a0d1c7165d3c83cc3f99ce2
ayushigeorge <76419649+ayushigeorge@users.noreply.github.com>
2021-07-10 16:58:08 +0530

    Create guessing game.py

---
+++ guessing game.py
@@ -0,0 +1,18 @@
+#guess the number game
+import random
+
+chosen = random.randint(1,100)
+print("Choose a number between 1 and 100")
+while True:
+    try:
+    answer = int(input("Enter yout number: "))
+    if (answer < chosen):
+        print("your guess is SMALLER than the chosen number")
+    elif (answer > chosen):
+        print("your guess is BIGGER than the chosen number")
+    else:
+        print("Great! You guessed it right" +str(chosen))
+        break
+        except:
+            print("Please enter a number only!")
+
