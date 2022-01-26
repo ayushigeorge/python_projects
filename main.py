2b4d9e512dfa6ed53a7f96fb34db5af374bf76e9
Ayushi George <ayushigeorge31@gmail.com>
2022-01-26 19:28:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+if number>= 0 and number%2==0:
+    print("The given number is positive and even")
+else:
+    print(" the given number is negative and odd")
+    
+
