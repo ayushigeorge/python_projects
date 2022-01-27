e351fcec97bed5de476d231830425bcd659977c7
Ayushi George <ayushigeorge31@gmail.com>
2022-01-27 20:08:48 +0530

    Add files via upload

---
+++ DAY18OF100.py
@@ -0,0 +1,10 @@
+#day 18 of 100 days of code!
+#converting list into dictionary, and vice versa, tuple into list and vice versa, dict into tuple and vice versa.
+
+#make a list
+student_list = ["ayushi", "aditi", "anglyn", "kesia", "steffi"]
+#making a dictionary in python
+student_dict = {stu : "passed"  for stu in student_list}
+print(student_dict)
+
+
