a70bdec1a502ad32aa23714c92af07ed8bc51f8a
Ayushi George <ayushigeorge31@gmail.com>
2022-02-16 19:32:10 +0530

    Add files via upload
    Python Project: Dice Simulator

---
+++ day36of100.py
@@ -0,0 +1,20 @@
+#Day 36 of 100
+#Python project: Dice Simulator
+
+
+pip install random
+import random as rd
+#range of dice
+min_value =1
+max_value =6
+
+roll_dice ="yes"
+while roll_dice ="yes" or roll_dice = "y":
+    print("The dice is rolling....")
+    print("The numbers are: ")
+    print(random,randint(min_value, max_value))
+    #for second value of dice
+    print(random, randint(min_value, max_value))
+    
+roll_dice = input("Roll the dice??")
+
