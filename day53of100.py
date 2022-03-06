d524562017afd7c5c315e2e917359492edcc1b52
Ayushi George <ayushigeorge31@gmail.com>
2022-03-06 21:42:18 +0530

    quicksort

---
+++ day53of100.py
@@ -0,0 +1,13 @@
+#DAY 53 of 100 
+#Made Quick SORT- Program in Python
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+print(quicksort([3,6,8,10,1,2,1]))
